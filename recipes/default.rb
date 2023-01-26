f9408a045b87064e090e11c3d0c0905e74624206
noahmmcgivern <noah.m.mcgivern@gmail.com>
2023-01-26 17:24:34 -0500

    feat: on host env vars

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,8 @@
 
 newrelic_install 'install' do
   action                   :install
-  new_relic_api_key        node['newrelic_install']['env']['NEW_RELIC_API_KEY']
-  new_relic_account_id     node['newrelic_install']['env']['NEW_RELIC_ACCOUNT_ID']
+  new_relic_api_key        node['newrelic_install']['NEW_RELIC_API_KEY']
+  new_relic_account_id     node['newrelic_install']['NEW_RELIC_ACCOUNT_ID']
+  new_relic_region         node['newrelic_install']['NEW_RELIC_REGION']
   env                      node['newrelic_install']['env']
 end

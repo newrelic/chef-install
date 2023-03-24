6d7f21f49379d4592ad4a5ee1235bfe1749267fa
hang zhao <hzhao@newrelic.com>
2023-03-24 10:32:32 -0700

    chore: unit test

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'chefspec'
+
+Dir['libraries/*.rb'].sort.each { |f| require File.expand_path(f) }
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
+
+RSpec.configure do |config|
+  config.color = true               # Use color in STDOUT
+  config.formatter = :documentation # Use the specified formatter
+  config.log_level = :error         # Avoid deprecation notice SPAM
+
+  # run all specs when using a filter, but no spec match
+  config.run_all_when_everything_filtered = true
+
+  Ohai::Config[:log_level] = :error
+end
